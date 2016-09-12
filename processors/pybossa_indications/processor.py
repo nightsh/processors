f7d5300ca78f4d5defa822209f460aac79e0e463
Victor Nițu <victor@thesponge.eu>
2016-09-12 15:35:17 +0300

    Coding style modifications. New gitignore section.
    TODO: Alter the query according to the new DB schema

--- processors/pybossa_indications/processor.py
+++ processors/pybossa_indications/processor.py
@@ -20,10 +20,6 @@ PROJECT_ID = os.environ['PYBOSSA_PROJECT_INDICATIONS']
 
 def process(conf, conn):
 
-    # Build up a limit so we'l get all the tasks in one go
-    offset = 0
-    bufsize = 10000
-
     # Get stuff from the DB
     query = """
         SELECT
@@ -39,17 +35,17 @@ def process(conf, conn):
         WHERE documents.url IS NOT NULL
         AND fda_approvals.type = 'New or Modified Indication'
         GROUP BY fda_approvals.id, documents.id
-        ORDER BY fda_approvals.id LIMIT %s OFFSET %s
+        ORDER BY fda_approvals.id
     """
 
-    rows = list(conn['database'].query(query % (bufsize, offset)))
-    if not rows:
+    rows = list(conn['database'].query(query))
+    if rows:
+        _create_tasks(rows)
+    else:
         logger.info('No new and modified indications found for tasks creation')
-        return
-    create_tasks(rows)
 
 
-def create_tasks(rows):
+def _create_tasks(rows):
     tasks = []
     for key, group in groupby(rows, lambda x: x['id']):
         task = {}
@@ -68,26 +64,25 @@ def create_tasks(rows):
         tasks.append(task)
     logger.debug('{} tasks in the database'.format(len(tasks)))
 
-    submit_tasks(tasks)
+    _submit_tasks(tasks)
 
 
-def pybossa_rate_limitation(endpoint):
+def _pybossa_rate_limitation(endpoint):
     # This should be called before actual requests to avoid getting HTTP 429s
-    res = requests.get('{}/api/task'.format(PYBOSSA_URL))
+    res = requests.get('{}/api/{}'.format(PYBOSSA_URL, endpoint))
     if int(res.headers['X-RateLimit-Remaining']) < 30:
         logger.warn('Rate limit reached, will sleep for 5 minutes')
-        time.sleep(300)  # Sleep for 2 minutes
-        return
+        time.sleep(300)  # Sleep for 5 minutes
 
 
-def get_existing_ids(PROJECT_ID):
+def _get_existing_ids(PROJECT_ID):
     existing_ids = []
     tasks = []
     offset = 0
     limit = 100
 
     # Make sure we have enough requests left
-    pybossa_rate_limitation('task')
+    _pybossa_rate_limitation('task')
     while True:
         collection = pbc.get_tasks(PROJECT_ID, limit=limit, offset=offset)
         if len(collection) > 0:
@@ -100,14 +95,14 @@ def get_existing_ids(PROJECT_ID):
     return existing_ids
 
 
-def submit_tasks(tasks):
+def _submit_tasks(tasks):
 
     # Set up PyBossa connection
     pbc.set('endpoint', PYBOSSA_URL)
     pbc.set('api_key', PYBOSSA_API_KEY)
 
     # Get existing IDs
-    existing_ids = get_existing_ids(PROJECT_ID)
+    existing_ids = _get_existing_ids(PROJECT_ID)
     if not isinstance(existing_ids, list):
         logger.error('Cannot get the list of existing task IDs')
 
@@ -116,11 +111,11 @@ def submit_tasks(tasks):
     logger.debug('{} tasks to be created'.format(len(cleaned_tasks)))
 
     for task in cleaned_tasks:
-        submit_task(task, PROJECT_ID)
+        _submit_task(task, PROJECT_ID)
 
 
-def submit_task(task, PROJECT_ID):
+def _submit_task(task, PROJECT_ID):
     res = pbc.create_task(PROJECT_ID, task)
     if isinstance(res, dict) and res['status_code'] == 429:
-        pybossa_rate_limitation('task')
-        submit_task(task, PROJECT_ID)
+        _pybossa_rate_limitation('task')
+        _submit_task(task, PROJECT_ID)
